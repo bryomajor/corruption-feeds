ed017d36c7f8aef871508b7fede6d1681ceafb37
Albert Byrone <albertbyrone1677@gmail.com>
2019-10-30 09:15:36 +0300

    add a nickname column to the user model

--- app/models.py
+++ app/models.py
@@ -11,6 +11,7 @@ class User(UserMixin,db.Model):
     username = db.Column(db.String(255),unique=True,nullable=False)
     email  = db.Column(db.String(255),unique = True,nullable = False)
     bio  = db.Column(db.String(255),default='My default Bio')
+    nickname  = db.Column(db.String(255),unique=True,nullable=False)
     profile_pic_path = db.Column(db.String(255),default='default.png')
     hashed_password = db.Column(db.String(255),nullable=False)
     case = db.relationship('Case',backref='user',lazy='dynamic')
