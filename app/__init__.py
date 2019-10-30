4fd7f9385ba09056b7e4c855871196e832df9d3f
Albert Byrone <albertbyrone1677@gmail.com>
2019-10-30 10:53:37 +0300

    minor updates

--- app/__init__.py
+++ app/__init__.py
@@ -18,8 +18,12 @@ login_manager.login_view = 'auth.login'
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config_options[config_name])
-    from .auth import auth as authentication_blueprint
+    from .auth import auth as auth_blueprint
     from .main import main as main_blueprint
+
+
+    app.register_blueprint(main_blueprint)
+    app.register_blueprint(auth_blueprint)
     
     bootstap.init_app(app)
 
