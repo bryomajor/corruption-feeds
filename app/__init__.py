862fa9f4547cbc0b2af6ecab6df5f735d188aa5d
Albert Byrone <albertbyrone1677@gmail.com>
2019-11-01 10:51:46 +0300

    remove space

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from config import config_options
 from flask_bootstrap import Bootstrap
 from flask_uploads import UploadSet, configure_uploads, IMAGES
-
 photos = UploadSet('photos', IMAGES)
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
