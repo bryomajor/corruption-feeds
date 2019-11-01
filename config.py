636a6b9a2a140fe473393069dfc076f4d15d6227
Albert Byrone <albertbyrone1677@gmail.com>
2019-11-01 09:42:47 +0300

    deployment to heroku

--- config.py
+++ config.py
@@ -2,14 +2,13 @@ import os
 class Config:
     UPLOADED_PHOTOS_DEST='app/static/photos'
     SECRET_KEY='noneofyb'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://byrone:Albert254@localhost/feeds'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://byrone:Albert254@localhost/feeds'
 
 class ProdConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI=os.environ.get("DATABASE_URI")
 
 class DevConfig(Config):
-    pass
-
+    
     DEBUG = True
 
 config_options = {
